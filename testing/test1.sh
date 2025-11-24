a740433bf29409561fb93f39bded8d25cd273ff4
reinoyk <reino.yk@gmail.com>
2025-11-24 16:27:45 +0700

    code: update ip and port on testing

--- testing/test1.sh
+++ testing/test1.sh
@@ -2,16 +2,16 @@
 #
 
 # Single PUT to node 1
-python3 ./kvclient.py --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003 cmd --node 1  "PUT color blue"
+python3 ./kvclient.py --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003 cmd --node 1  "PUT color blue"
 
 
 # GET from node 2
-python3 ./kvclient.py  --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003 cmd --node 2   "GET color"
+python3 ./kvclient.py  --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003 cmd --node 2   "GET color"
 
 
 # Race two writers (great for no-mutex demo)
-python3 ./kvclient.py --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003 race  "PUT color blue" "PUT color red"
+python3 ./kvclient.py --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003 race  "PUT color blue" "PUT color red"
 
 
 # Read the key from ALL nodes after the race
-python3 ./kvclient.py --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003  getall color
+python3 ./kvclient.py --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003  getall color
--- testing/test3.sh
+++ testing/test3.sh
@@ -2,15 +2,15 @@
 #
 
 # Single PUT to node 1
-python3 ./kvclient.py --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003 cmd --node 1  "PUT color black"
-python3 ./kvclient.py --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003 cmd --node 2  "PUT color magenta"
-python3 ./kvclient.py --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003 cmd --node 2  "PUT warna kuning"
-python3 ./kvclient.py --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003 cmd --node 2  "PUT warna kuning"
-python3 ./kvclient.py --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003 race "PUT color blue" "PUT color green" 
-python3 ./kvclient.py --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003 race "PUT warna oranye" "PUT warna hijau" 
+python3 ./kvclient.py --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003 cmd --node 1  "PUT color black"
+python3 ./kvclient.py --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003 cmd --node 2  "PUT color magenta"
+python3 ./kvclient.py --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003 cmd --node 2  "PUT warna kuning"
+python3 ./kvclient.py --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003 cmd --node 2  "PUT warna kuning"
+python3 ./kvclient.py --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003 race "PUT color blue" "PUT color green" 
+python3 ./kvclient.py --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003 race "PUT warna oranye" "PUT warna hijau" 
 
 
 echo "-------HASIL----"
-python3 ./kvclient.py --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003  getall color
+python3 ./kvclient.py --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003  getall color
 echo "-------HASIL----"
-python3 ./kvclient.py --nodes 192.168.122.160:5001,192.168.122.188:5002,192.168.122.161:5003  getall warna
+python3 ./kvclient.py --nodes 192.168.122.161:5001,192.168.122.188:5002,192.168.122.160:5003  getall warna
